45b1a3ef27a0290ebffad82cfb90728e3dcb2f89
navyanaveli <navyanaveli1234@gmail.com>
2021-02-22 10:52:03 +0530

    add implementation

---
+++ src/components/StalkPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="enter">
+    <img id="git" src="https://miro.medium.com/max/1400/1*WaaXnUvhvrswhBJSw4YTuQ.png">
+    <div>
+    <input  v-model="username" type="text" placeholder="Enter github username" v-on:keyup="addProfileSuggestions" >
+    <button @click="addProfileSuggestions">
+      Add
+    </button>
+    </div>
+    <ul>
+      <div >
+      <li id ="list" v-for="profileSuggestion in profileSuggestions" :key="profileSuggestion.index" @click="viewUserDetails(profileSuggestion.login)" >
+        <img alt="profile image" :src="profileSuggestion.avatar_url"/>
+        {{profileSuggestion.login}}
+
+      </li>
+        </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "StalkPage",
+  data: () => {
+    return {
+      username: "",
+      profileSuggestions: []
+    }
+  },
+  methods:{
+    addProfileSuggestions : function (){
+      axios.get('https://api.github.com/search/users?q='+this.username)
+      .then(response => {
+         this.profileSuggestions = response.data.items || [];
+         this.$router.push({query: { 'user': this.username }})
+      })
+    },
+    viewUserDetails : function (login){
+      this.$router.push({name:'UserDetails',params:{user:login}});
+    }
+  },
+  created() {
+    this.username=this.$route.query.user || "";
+    this.addProfileSuggestions();
+  }
+
+}
+</script>
+
+<style scoped>
+#list{
+  padding: 10px;
+  background-color: palegoldenrod;
+  border: 2px solid darkgray;
+  border-collapse: separate;
+}
+li{
+  list-style-type: none;
+}
+#git{
+  height: 500px;
+
+}
+
+img {
+ height: 50px;
+}
+
+</style>
---
+++ src/components/UserDetails.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <img :src="userDetail.avatar_url"/>
+  <h1>Welcome to {{username}}'s detail!</h1>
+  <h2>User Id: {{userDetail.id}}</h2>
+  <h2>User Name: {{userDetail.name}}</h2>
+  <h2>No. of followers: {{userDetail.followers}}</h2>
+  <h2>No.of following: {{userDetail.following}}</h2>
+  <h2 v-if="userDetail.location">Location: {{userDetail.location}}</h2>
+
+</div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "UserDetails",
+  data: ()=> {
+    return{
+      username: "",
+      userDetail:{}
+    }
+  },
+  created() {
+    this.username=this.$route.params.user
+
+    axios.get('https://api.github.com/users/'+this.username)
+        .then(response => {
+          this.userDetail = response.data || {};
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
